29033bd70b1c6613263385da64d8032eb6efcdb0
Harshitcoding <khandelwalharshit96@gmail.com>
2025-04-04 16:23:10 +0530

    added auth

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
+import Header from "@/components/Header";
 import Landing from "@/components/Landing";
-import Image from "next/image";
 
 export default function Home() {
   return (
     <div>
+      <Header/>
       <Landing/>
     </div>
   );
---
+++ components/Header.tsx
@@ -0,0 +1,30 @@
+'use client'
+import { signIn, useSession } from "next-auth/react"
+import { Button } from "./ui/button"
+
+const Header = () => {
+  const { data: session, status } = useSession()
+
+  return (
+    <div className="flex justify-around items-center py-4">
+      <div className="text-4xl font-bold">
+        JobViews
+      </div>
+      <div>
+        {status === "loading" ? (
+          <div className="animate-spin border-4 border-t-transparent border-blue-500 rounded-full w-6 h-6"></div>
+        ) : session?.user ? (
+          <img
+            src={session.user.image || '/default-avatar.png'}
+            alt="Profile"
+            className="w-10 h-10 rounded-full"
+          />
+        ) : (
+          <Button onClick={() => signIn('google')}>Signin</Button>
+        )}
+      </div>
+    </div>
+  )
+}
+
+export default Header
